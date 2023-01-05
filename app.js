02ab542cc7d6b103777e979db87c155abda95c09
azxki <azxki01@gmail.com>
2023-01-05 09:35:53 +0100

    exo

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
-//Reset CSS
-document.body.style.margin = "0";
-document.body.style.padding = "0";
-document.body.style.boxSizing = "border-box";
+const slideCreate = document.createElement('li');
+const slideCreate2 = document.createElement('li');
+const slideCreate3 = document.createElement('li');
+const slideCreate4 = document.createElement('li');
+const slidesContainer = document.getElementById("slides-ul");
+const prevButton = document.getElementById("slide0-bef");
+const nextButton = document.getElementById("slide0-ap");
+slidesContainer.appendChild(slideCreate);
+slidesContainer.appendChild(slideCreate2);
+slidesContainer.appendChild(slideCreate3);
+slidesContainer.appendChild(slideCreate4);
+slideCreate.className = "slide";
+slideCreate2.className = "slide";
+slideCreate3.className = "slide";
+slideCreate4.className = "slide";
+const slide = document.querySelector(".slide");
 
-// Mes divs
-div = document.createElement('div');
-div2 = document.createElement('div');
-document.body.appendChild(div);
-document.body.appendChild(div2);
-div.id = "img1";
-div2.id = "img2";
+slideCreate.style.backgroundColor = "#efec1f";
+slideCreate2.style.backgroundColor = "#8232b0";
+slideCreate3.style.backgroundColor = "#47608f";
+slideCreate4.style.backgroundColor = "#4c883f";
+nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
 
-//Récupération des divs
-monDiv = document.getElementById('img1');
-monDiv2 = document.getElementById('img2');
-
-//Mes images
-img1 = document.createElement('img');
-img2 = document.createElement('img');
-monDiv.appendChild(img1);
-monDiv2.appendChild(img2);
-
-//Affichage des images
-img1.src = "01.jfif";
-img2.src = "02.jfif";
-
-//Placement de mes images
-//img1
-img1.style.position = "absolute";
-img1.style.width = "50%";
-img1.style.marginLeft = "25%";
-img1.style.marginTop = "12%";
-//img2
-img2.style.position = "absolute";
-img2.style.width = "50%";
-img2.style.marginLeft = "110%";
-img2.style.marginTop = "12%";
-
-//Les transitions
-//img1
-img1.style.transition
-//img2
+prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+});
